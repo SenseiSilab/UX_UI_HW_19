24cabaccce3f6a10bf8adabdafbc69ddf0f84d89
SenseiSilab <131554749+SenseiSilab@users.noreply.github.com>
2023-09-27 17:37:11 -0400

    Added hover states

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Your index.js file is loaded correctly!")
+console.log("Your index.js file is loaded correctly!");
+
+/*translate image and introduction statement into frame 
+when user lands on the homepage*/
+
+
+/*fade in for project cards*/
+$(document).ready(function(){
+    $("button.projecsButton").click(function(){
+      $(".caseStudy1").fadeIn();
+      $(".caseStudy2").fadeIn("slow");
+      $(".caseStudy3").fadeIn(3000);
+    });
+  });
+
